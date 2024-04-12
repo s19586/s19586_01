0f12a000e4ebf92ef52a103e32ca90c2d2926751
s19586 <s19586@pjwstk.edu.pl>
2024-04-12 15:57:51 +0200

    implemented feature average

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("To jest trzecia modyfikacja");
+﻿using System;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("To jest trzecia modyfikacja");
+
+        var arr = new int[] { 10, 20, 40 };
+        CalculateAndPrintAverage(arr);
+
+    }
+
+    public static void CalculateAndPrintAverage(int[] arr)
+    {
+        double avg = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            avg += arr[i];
+        }
+        avg /= arr.Length;
+        Console.WriteLine("Average: " + avg);
+    }
+}
